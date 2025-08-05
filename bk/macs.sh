3e8158d963c849438391ee24de32925256cbee86
Meiyuan Ji <jimeiyuan93@gmail.com>
2025-08-05 11:16:29 -0400

    stop tracking unwanted files

--- bk/macs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# input: bam, outdir
-ml macs/3
-
-name=$(basename $1)
-name=${name%.bam}
-
-macs3 callpeak --keep-dup all -t $1 -f BAMPE --outdir $2 -q 0.01 --nomodel --shift -75 --extsize 150 -n $name
-
--- bk/no_warning.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-if not sys.warnoptions:
-    import warnings
-    warnings.simplefilter("ignore")
-
