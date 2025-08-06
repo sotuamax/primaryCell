4d92788f45e51dd85daa25b7912e226a727f4d16
Meiyuan Ji <jimeiyuan93@gmail.com>
2025-08-06 12:36:30 -0400

    examine local maxima for upstream and downstream

--- insulation.py
+++ insulation.py
@@ -13,14 +13,14 @@ valley: the point where derivative as zero and where the local minima reached;
 strength: difference between the valley point derivative and its neighboring point derivative; 
 
 update: 
-derivative: 3 bins on each side -> as an argument (default : 3)
-minima: derivative 0 and also the score is minima in surrounding 5 bin on each side -> as an argument (default : 10)
-minma screen should examine regions beyond the shifting site (-1, +1)
+derivative: 3 bins on each side -> as an argument (-dv, default : 3)
+minima: the score is minima in surrounding 5 bin on each side -> as an argument (-vs, default : 10); minma screen should examine regions beyond the shifting site (-1, +1)
+surrounding region should be at least >fd times of the local minima; 
 
 """
 
 # import cooler 
-# import bioframe as bf 
+import bioframe as bf 
 import numpy as np
 import pandas as pd
 import argparse
@@ -30,6 +30,7 @@ from utilities.chrom_func import chrom_arms
 from utilities.misc import ignore_warning
 from tqdm.auto import tqdm
 import os 
+
 ignore_warning()
 
 def args_parser():
@@ -41,8 +42,8 @@ def args_parser():
     parser.add_argument("-r", "--resolution", help = "resolution", type = int)
     parser.add_argument("-w", "--window", nargs = "+", help = "window size used to calculate submatrix score (mean of contacts)", type = int)
     parser.add_argument("-dv", "--derivative", type = int, default = 3, help = "bin size used to calculate derivative for center position")
-    parser.add_argument("-v", "--valley_screen", type = int, default = 10, help = "To find minima of a screening size (per side)")
-    parser.add_argument("-fd", "--foldchange", default = 1.8, type = float, help = "within the screen range, the maximum value is *fd* times of the minima score. ")
+    #parser.add_argument("-vs", "--valley_screen", type = int, default = 10, help = "To find minima of a screening size (per side)")
+    parser.add_argument("-fc", "--foldchange", default = 1.5, type = float, help = "within the screen range, the maximum value is *fd* times of the minima score. ")
     parser.add_argument("-o", "--output", help = "output file prefix name")
     args = parser.parse_args()
     return args
@@ -58,13 +59,12 @@ def derivative(arr, side):
         new_arr[i] = np.mean(after) - np.mean(before)
     return new_arr 
 
-def valley_point(arr_slope, arr_value, v, fd):
+def valley_point(arr_slope, arr_value):
     """
     pick for two consecutive derivatives crossing zero (which can be peak/valley); 
     trend from negative derivative to positive derivative as the region surrounding local minima (valley);
     """
-    # 0s as initiate array 
-    valley_arr = np.zeros(len(arr_slope))
+    # valley_max_downstream = np.zeros(len(arr_slope))
     #strength_arr = np.zeros(len(arr_slope))
     #minima_arr = np.zeros(len(arr_slope))
     signs = np.sign(arr_slope) # get the sign of slope
@@ -73,11 +73,29 @@ def valley_point(arr_slope, arr_value, v, fd):
     # find shifting derivatives (from negative to positive) where match to a valley, and return index
     valley_zero = zero_crossing[arr_slope[zero_crossing] < arr_slope[zero_crossing+1]] 
     # find neighboring points surrounding derivative at 0, and then returning the one point closest to 0 
-    valley_update = np.array([i - 2 + np.argmin(arr_value[i-2:i+4]) for i in valley_zero]) # key improve to expand region for searching of minima around the derivative shifting site 
-    valley_update2 = np.array([i for i in valley_update if i > v and np.max(arr_value[i-v:i+v+1]) >= fd*(arr_value[i])])
-    valley_arr[valley_update2] = 1 # replace 1 at index for valley
-    # strength_arr[valley_update2] = arr_slope[valley_zero+1] - arr_slope[valley_zero] # add strength value at index for valley (slope change)
-    return valley_arr #, strength_arr
+    valley_index = np.unique(np.array([0] + [i - 2 + np.argmin(arr_value[i-2:i+4]) for i in valley_zero if i > 2] + [len(arr_slope)])).astype(int) # key improve to expand region for searching of minima around the derivative shifting site 
+    # to avoid same index occurs twice, add unique for the index value 
+    # for a,b in list(zip(valley_index[:-1], valley_index[1:])):
+    #     print(a,b, np.max(arr_value[a:b]))
+    upstream_max = np.array([np.max(arr_value[a:b]) for a,b in list(zip(valley_index[:-2], valley_index[1:-1]))])
+    downstream_max = np.array([np.max(arr_value[a:b]) for a,b in list(zip(valley_index[1:-1], valley_index[2:]))])
+    valley_min = np.zeros(len(arr_slope)); valley_min[valley_index[1:-1]] = 1 # replace 1 at index for minima 
+    valley_upstream = np.zeros(len(arr_slope)); valley_upstream[valley_index[1:-1]] = upstream_max
+    valley_downstream = np.zeros(len(arr_slope)); valley_downstream[valley_index[1:-1]] = downstream_max
+    # strength_arr[valley_index2] = arr_slope[valley_zero+1] - arr_slope[valley_zero] # add strength value at index for valley (slope change)
+    return valley_min, valley_upstream, valley_downstream #, strength_arr
+
+def update_max(valley_value, arr_value):
+    valley_index = np.where(valley_value == 1)[0]
+    valley_upmax = np.zeros(len(arr_value)); valley_downmax = np.zeros(len(arr_value))
+    if len(valley_index) > 0:
+        if valley_index[0] != 0:
+            valley_index = np.insert(valley_index, 0,0)
+            valley_index = np.insert(valley_index, -1, len(valley_value))
+            valley_index = np.unique(valley_index)
+        valley_upmax[valley_index[1:-1]] = np.array([np.max(arr_value[a:b]) for a,b in list(zip(valley_index[:-2], valley_index[1:-1]))])
+        valley_downmax[valley_index[1:-1]] = np.array([np.max(arr_value[a:b]) for a,b in list(zip(valley_index[1:-1], valley_index[2:]))])
+    return valley_upmax, valley_downmax 
 
 def main():
     from mpi4py import MPI
@@ -88,11 +106,12 @@ def main():
     size = comm.Get_size()
     args = args_parser()
     cool = args.cool 
+    fc = args.foldchange
     #from utilities.cool_tools import parser_cool 
     #cool_handle = parser_cool(cool, args.resolution)
     resolution = args.resolution
     ref = args.reference 
-    out = args.output 
+    out = args.output
     ref_arm = chrom_arms(ref)
     # clr = parser_cool(cool, resolution)
     # only intra-chromosome (cis) PETs under consideration
@@ -112,12 +131,12 @@ def main():
     worker_tasks = comm.bcast(worker_tasks, root = 0)
     for win in win_list:
         pixel_list = list()
-        for i in worker_tasks[rank]: # each core process one chrom-arm 
+        for i in tqdm(worker_tasks[rank], desc = f"Parallel: {rank}"): # each core process one chrom-arm 
             chr,start,end = ref_arm.loc[i,"chrom"], ref_arm.loc[i,"start"], ref_arm.loc[i,"end"]
             i_offset = start//resolution
             region_mtx = cool_matrix(cool, resolution, region = (chr, start, end))
             # = ref_row.chrom, ref_row.start, ref_row.end 
-            for center in tqdm(range(start//resolution+win-i_offset, end//resolution-win-i_offset, 1), desc = f"Process:{chr}:{start}-{end}", position = 1, leave = True):
+            for center in tqdm(range(start//resolution+win-i_offset, end//resolution-win-i_offset, 1), desc = f"Process:{chr}:{start}-{end}", position = 1, leave = False):
                 region_submtx = region_mtx[center-win:center, center+1:center+win+1]
                 count = region_submtx.mean()
                 size = (region_submtx > 0).sum()
@@ -128,44 +147,60 @@ def main():
             print("Process window: ", f"{win}x{win}")
             print("Collecting all insulation score ...")
             pixel_all_df = pd.concat(pixel_all, axis = 0)
-            # for w in win_list:
-            #     pixel_all_df = pixel_all_df[pixel_all_df[f"size_{w}"] > w*w*0.3]
-            # pixel_all_df.sort_values(by = ["chrom", "start"]).to_csv(f"{out}.txt", sep = "\t", header = True, index = False)
+            pixel_all_df = pixel_all_df[pixel_all_df[f"size_{win}"] > win].copy()
             c_new_list = list()
-            for _, c_df in pixel_all_df.groupby("chrom"):
-                c_df[f"slope_{win}"] = derivative(np.array(c_df[f"score_{win}"]), args.derivative)
+            for row in ref_arm.itertuples():
+                c_df = bf.select(pixel_all_df, (row.chrom, row.start, row.end))
+                c_df[f"Df_{win}"] = derivative(np.array(c_df[f"score_{win}"]), args.derivative)
                 c_new_list.append(c_df)
             c_new = pd.concat(c_new_list, axis = 0)
             c_new2 = list()
-            for _, c_df in c_new.groupby("chrom"):#,c_df[f"strength_{win}"]
-                c_df[f"valley_{win}"] = valley_point(np.array(c_df[f"slope_{win}"]), np.array(c_df[f"score_{win}"]), args.valley_screen, args.foldchange)
+            for row in ref_arm.itertuples():
+                c_df = bf.select(c_new, (row.chrom, row.start, row.end))
+                c_df[f"valley_{win}"],c_df[f"upmax_{win}"], c_df[f"downmax_{win}"] = valley_point(np.array(c_df[f"Df_{win}"]), np.array(c_df[f"score_{win}"]))
                 c_new2.append(c_df)
             c_new2_df = pd.concat(c_new2, axis = 0)
+            c_new2_df[f"upfc_{win}"] = c_new2_df[f"upmax_{win}"]/c_new2_df[f"score_{win}"]; c_new2_df[f"downfc_{win}"] = c_new2_df[f"downmax_{win}"]/c_new2_df[f"score_{win}"]
+            #c_new2_df.to_csv(f"{out}_{win}.tmp", sep = "\t", header = True, index = False)
+            #exit(0)
+            # iterative update on maxima value surrounding minima valley 
+            c_validate = c_new2_df[c_new2_df[f"valley_{win}"] == 1].copy()
+            while len(c_validate[(c_validate[f"upfc_{win}"] < fc) & (c_validate[f"downfc_{win}"] < fc)]) > 0:
+                print("Iteration run ...")
+                c_new2_df[f"valley_{win}"] = np.where((c_new2_df[f"upfc_{win}"] < fc) & (c_new2_df[f"downfc_{win}"] < fc), 0 , c_new2_df[f"valley_{win}"])
+                c_list = list()
+                for row in ref_arm.itertuples():
+                    c_df = bf.select(c_new2_df, (row.chrom, row.start, row.end))
+                    c_df[f"upmax_{win}"], c_df[f"downmax_{win}"] = update_max(np.array(c_df[f"valley_{win}"]), np.array(c_df[f"score_{win}"]))
+                    c_list.append(c_df)
+                c_new2_df = pd.concat(c_list, axis = 0)
+                c_new2_df[f"upfc_{win}"] = c_new2_df[f"upmax_{win}"]/c_new2_df[f"score_{win}"]; c_new2_df[f"downfc_{win}"] = c_new2_df[f"downmax_{win}"]/c_new2_df[f"score_{win}"]
+                c_validate = c_new2_df[c_new2_df[f"valley_{win}"] == 1]
             c_new2_df.to_csv(f"{out}_{win}.txt", sep = "\t", header = True, index = False)
     if rank == 0:
         win_file = list()
         for win in win_list:
             c_win = pd.read_table(f"{out}_{win}.txt", sep = "\t", header = 0, index_col = ["chrom", "start", "end"])
-            #c_valley = c_win[c_win[f"valley_{win}"] == 1]
-            #strength_q5 = np.quantile(np.log10(c_valley[f"strength_{win}"]), 0.05)
-            #c_valley[np.log10(c_valley[f"strength_{win}"]) > strength_q5][[f"strength_{win}"]].to_csv(f"{win}.bedGraph", header = False, index = True, sep = "\t")
             win_file.append(c_win)
-        all_win = pd.concat(win_file, axis = 1, join = "inner")
-        all_win.to_csv(f"{out}.txt", sep = "\t", header = True, index = True)
+        all_win = pd.concat(win_file, axis = 1, join = "inner"); all_win.reset_index(inplace = True)
+        all_win.to_csv(f"{out}.txt", sep = "\t", header = True, index = False)
         if os.path.exists(f"{out}.txt"):
             try:
                 for win in win_list:
                     os.remove(f"{out}_{win}.txt")
             except:
                 pass
+        col = [c for c in all_win.columns if c.startswith("valley")]
+        select_win = all_win[(all_win[col] == 1).mean(axis = 1) == 1].copy()
+        print("Generating boundary bedGraph file (foldchange as )...")
+        for win in win_list:
+            select_win[f"fc_{win}"] = np.where(select_win[f"upfc_{win}"] > select_win[f"downfc_{win}"], select_win[f"upfc_{win}"], select_win[f"downfc_{win}"])
+            select_win[f"strand_{win}"] = np.where((select_win[f"upfc_{win}"] > select_win[f"downfc_{win}"]) & (select_win[f"downfc_{win}"] < fc), "+", np.where((select_win[f"upfc_{win}"] < select_win[f"downfc_{win}"]) & (select_win[f"upfc_{win}"] < fc), "-", "."))
+        select_win["fc"] = select_win[[c for c in select_win.columns if c.startswith("fc")]].mean(axis = 1)
+        # print(select_win)
+        select_win["name"] = range(len(select_win)); select_win["name"] = "b" + select_win["name"].astype(str)
+        select_win[["chrom", "start", "end", "name", "fc", f"strand_{win}"]].to_csv(f"{out}.bed", sep = "\t", header = False, index = False)
     exit(0)
-        # valley_df = c_new2_df.query("valley == 1").copy()
-        # valley_df["pos1"] = valley_df["start"] + (resolution*2)/valley_df["strength"] * abs(valley_df["slope"])
-        # valley_df["pos2"] = valley_df["pos1"] + resolution
-        # valley_df["pos1"] = valley_df["pos1"].astype(int); valley_df["pos2"] = valley_df["pos2"].astype(int)
-        # valley_df.drop("valley", axis = 1, inplace = True)
-        # valley_df.to_csv(f"{out}.txt", sep = "\t", header = True, index = False)
-        # valley_df[["chrom", "pos1", "pos2", "strength"]].to_csv(f"{out}.bedGraph", sep = "\t", header = False, index = False)
         
 if __name__ == "__main__":
     main()
